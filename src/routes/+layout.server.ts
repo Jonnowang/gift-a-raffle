8db9120f59bdc01e160e21bbaf12ad5ed4cb9290
Jonathan Wang <38340825+Jonnowang@users.noreply.github.com>
2025-11-19 09:16:30 +0000

    Update daily to 7am

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -16,15 +16,15 @@ export const load: LayoutServerLoad = async () => {
 		const now = new Date();
 		const lastTicketDate = lastTicketDateStr ? new Date(lastTicketDateStr) : null;
 
-		const TEN_HOURS_IN_MS = 10 * 3600 * 1000;
+		const SEVEN_HOURS_IN_MS = 7 * 3600 * 1000;
 		const ONE_DAY_IN_MS = 24 * 3600 * 1000;
 
-		const currentTicketDayNumber = Math.floor((now.getTime() - TEN_HOURS_IN_MS) / ONE_DAY_IN_MS);
+		const currentTicketDayNumber = Math.floor((now.getTime() - SEVEN_HOURS_IN_MS) / ONE_DAY_IN_MS);
 		const lastTicketDayNumber = lastTicketDate
-			? Math.floor((lastTicketDate.getTime() - TEN_HOURS_IN_MS) / ONE_DAY_IN_MS)
+			? Math.floor((lastTicketDate.getTime() - SEVEN_HOURS_IN_MS) / ONE_DAY_IN_MS)
 			: -1;
 
-		if (now.getUTCHours() >= 10 && currentTicketDayNumber > lastTicketDayNumber) {
+		if (now.getUTCHours() >= 7 && currentTicketDayNumber > lastTicketDayNumber) {
 			await addTicket();
 			await setLastTicketDate(now.toISOString());
 			dailyTicket = true;
